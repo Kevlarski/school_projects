88befa7b04fe1098166688f36eec94f99fb3ff64
Kevlarski <100644041+Kevlarski@users.noreply.github.com>
2022-04-04 12:11:46 -0400

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,17 @@
+//Name: Kevin McLaughlin
+//Purpose: To introduce Myself
+//Class: CIS 1500
+
+public class Program1
+{
+   public static void main(String[]args)
+   {
+
+         System.out.println("My name is Kevin McLaughlin");
+         System.out.println("I am excited to learn a programming language!");
+         System.out.println("Today is Tuesday January 11th");
+         System.out.println("This is my first class");
+	 System.out.println("Today is very cold");
+
+   }
+}
