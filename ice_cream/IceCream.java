28d7bda921f4627aae0f8a5d4312bf77883ef96a
Kevlarski <100644041+Kevlarski@users.noreply.github.com>
2022-05-20 23:44:42 -0400

    Update IceCream.java
    minor aesthetic change

--- ice_cream/IceCream.java
+++ ice_cream/IceCream.java
@@ -10,8 +10,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
+/*
  * @author kevin
  */
 public class IceCream extends Application {
