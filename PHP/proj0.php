c074b5d4fd380b3b86e3555e36bff6e84e39b885
Kevlarski <kevinmclaughlin007@gmail.com>
2022-09-19 23:47:51 -0400

    added Assignment 4

--- PHP/proj0.php
+++ PHP/proj0.php
@@ -5,20 +5,23 @@
     $gross_inc = htmlspecialchars(filter_input(INPUT_POST, 'gross_inc', FILTER_VALIDATE_INT));
     $deductions= htmlspecialchars(filter_input(INPUT_POST, 'deductions', FILTER_VALIDATE_INT));
     
-    $adjusted_inc = number_format(($gross_inc)-($deductions),2);
+    if($deductions<12950){$deductions = 12950;}
     
-    if($adjusted_inc<=10275){
-        if($adjusted_inc===10275){
+    $adjusted_inc =(int) $gross_inc-$deductions;
+    
+    //if statements to establish taxed amounts
+    if($adjusted_inc <= 10275){
+        if($adjusted_inc === 10275){
             $ten = 1027.5;
         }
         else{
             $ten = ($adjusted_inc)*.1;
         }
     }//if <=
-    
-    if($adjusted_inc>10275){
-        if($adjusted_inc<41775){
-            $twelve = ($adjusted_inc)*.12;
+
+    if($adjusted_inc > 10275){
+        if($adjusted_inc < 41775){
+            $twelve = ($adjusted_inc - 10275) * .12;
             $ten = 1027.5;
         }
         else{
@@ -26,49 +29,50 @@
             $ten = 1027.5;
         }
     }//if >10275
-    
-    if($adjusted_inc>41775){
-        if($adjusted_inc<89075){
-            $twentytwo = ($adjusted_inc)*.22;
+
+    if($adjusted_inc > 41775){
+        if($adjusted_inc < 89075){
+            $twentytwo = ($adjusted_inc - 41775) * .22;
         }
         else{
             $twentytwo = 10406;
         }
     }//if >41775
-    
-    if($adjusted_inc>89075){
-        if($adjusted_inc<170050){
-            $twentyfour = ($adjusted_inc)*.24;
+
+    if($adjusted_inc > 89075){
+        if($adjusted_inc < 170050){
+            $twentyfour = ($adjusted_inc - 89075) * .24;
         }
         else{
             $twentyfour = 19434;
         }
     }//if >89075
-    
-    if($adjusted_inc>170050){
-        if($adjusted_inc<215950){
-            $thirtytwo = ($adjusted_inc)*.32;
+
+    if($adjusted_inc > 170050){
+        if($adjusted_inc < 215950){
+            $thirtytwo = ($adjusted_inc - 170050) * .32;
         }
         else{
             $thirtytwo = 14688;
         }
     }//if >170050
-    
-    if($adjusted_inc>215950){
-        if($adjusted_inc<539900){
-            $thirtyfive = ($adjusted_inc)*35;
+
+    if($adjusted_inc > 215950){
+        if($adjusted_inc < 539900){
+            $thirtyfive = ($adjusted_inc - 215950) * .35;
         }
         else{
             $thirtyfive = 113382.5;
         }
     }//if >215950
-    
-    if($adjusted_inc>=539900){
-        $thirtyseven = ($adjusted_inc)*.37;
+
+    if($adjusted_inc >= 539900){
+        $thirtyseven = ($adjusted_inc - 539900) * .37;
     }//if >539900
     
-    $tax_total = number_format(($ten+$twelve+$twentytwo+$twentyfour+$thirtytwo+
-            $thirtyfive+$thirtyseven),2);
+    //tax total and percentages
+    $tax_total = $ten+$twelve+$twentytwo+$twentyfour+$thirtytwo+
+            $thirtyfive+$thirtyseven;
     $gross_tax_per = ($tax_total/$gross_inc)*100;
     $adj_tax_per = ($tax_total/$adjusted_inc)*100;
     
@@ -79,6 +83,7 @@
     $deduction_str = "Total Deductions: $";
     $agi_str = "Adjusted Gross Income: $";
     $tax_tot_str = "Total Taxes Owed: $";
+    $taxes_per_str = "Taxes Owed as percentage of ";
 ?>
 
 <!DOCTYPE html>
@@ -110,7 +115,7 @@
             echo "<h2>Tax Calculator Results for $first_name $last_name</h2>";
             echo $gross_str.number_format($gross_inc,2)."<br/>";
             echo $deduction_str.number_format($deductions,2)."<br/>";
-            echo $agi_str.$adjusted_inc."<br/>";
+            echo $agi_str. number_format($adjusted_inc,2)."<br/>";
             echo $taxes_owed_str."10".$bracket_str.number_format($ten,2)."<br/>";
             echo $taxes_owed_str."12".$bracket_str.number_format($twelve,2)."<br/>";
             echo $taxes_owed_str."22".$bracket_str.number_format($twentytwo,2)."<br/>";
@@ -118,9 +123,9 @@
             echo $taxes_owed_str."32".$bracket_str.number_format($thirtytwo,2)."<br/>";
             echo $taxes_owed_str."35".$bracket_str.number_format($thirtyfive,2)."<br/>";
             echo $taxes_owed_str."37".$bracket_str.number_format($thirtyseven,2)."<br/>";
-            echo $tax_tot_str.$tax_total."<br/>";
-            echo "Taxes owed as percentage of gross income: ".$gross_tax_per."%<br/>";
-            echo "Taxes Owed as percentage of adjusted gross income: ".$adj_tax_per."%<br/>";
+            echo $tax_tot_str. number_format($tax_total,2)."<br/>";
+            echo $taxes_per_str."Gross Income: ". number_format($gross_tax_per,2)."%<br/>";
+            echo $taxes_per_str."Adjusted Gross Income: ". number_format($adj_tax_per,2)."%<br/>";
         ?>
     </body>
 </html>
